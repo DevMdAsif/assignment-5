5181a1c3e26c7f53ce75666d6f83c7b83d2ff33f
MD.Asif Ullah <mdasifullah456@gmail.com>
2023-10-05 10:58:59 +0600

    popular products section added

--- src/script.js
+++ src/script.js
@@ -1,3 +1,20 @@
+const imageArray = [
+    "../src/image/360-36.jpg",
+    "./image/11-shop_919cff98-ebe7-42f2-9e85-42043e3c883f.png",
+    "./image/14-shop.png",
+    "./image/6-shop_9b796b46-d45e-4151-8946-e2652ec7eed4.png",
+    "./image/8-shop_202894e6-7925-40b3-9a94-47f3972a188a.png",
+    "./image/6-shop_9b796b46-d45e-4151-8946-e2652ec7eed4.png",
+    "./image/14-shop.png",
+    "./image/11-shop_919cff98-ebe7-42f2-9e85-42043e3c883f.png",
+   
+  ];
+
+  const productBox = document.querySelectorAll(".L-product-box");
+const product = document.querySelectorAll(".L-product");
+
+
+
 productBox.forEach((element, index) => {
     let orignalImage = product[index].src;
     product[index].addEventListener("mouseover", () => {
